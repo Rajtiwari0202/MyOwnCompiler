bd6b4d99ac5cb9ab65c6e5b226f2212d6f7eba1e
Rajtiwari0202 <rajtiwari16916@gmail.com>
2026-07-26 21:52:25 +0530

    Cleanup: restore compiler entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
-#include "lexer/token.h"
 
 int main()
 {
-    Token token(
-        TokenType::Identifier,
-        "age"
-    );
-
-    std::cout << token.lexeme << "\n";
-
+    std::cout << "MyOwnCompiler v0.1\n";
     return 0;
 }
